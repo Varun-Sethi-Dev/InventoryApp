b535b7963e1b8dd8b98bb0eb8ba46a5ec60047d7
Varun Sethi <this.is.work.varun@gmail.com>
2024-02-14 12:07:56 +0530

    2.0

--- app/src/main/java/com/example/inventory/ui/item/ItemDetailsViewModel.kt
+++ app/src/main/java/com/example/inventory/ui/item/ItemDetailsViewModel.kt
@@ -18,14 +18,32 @@ package com.example.inventory.ui.item
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.inventory.data.ItemsRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 
 /**
  * ViewModel to retrieve, update and delete an item from the [ItemsRepository]'s data source.
  */
 class ItemDetailsViewModel(
-    savedStateHandle: SavedStateHandle
+    savedStateHandle: SavedStateHandle,
+    private val itemRepository: ItemsRepository
 ) : ViewModel() {
+    val uiState: StateFlow<ItemDetailsUiState> =
+        itemRepository.getItemStream(id = itemId).filterNotNull()
+            .map {
+                ItemDetailsUiState(itemDetails = it.toItemDetails())
+            }.stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
+                initialValue = ItemDetailsUiState()
+
+            )
 
     private val itemId: Int = checkNotNull(savedStateHandle[ItemDetailsDestination.itemIdArg])
 
